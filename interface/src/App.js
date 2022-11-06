60166a885e75e7783eb1db85af5814841eb39e93
SandyYusuf <sandyyusuf0@gmail.com>
2022-11-06 11:48:56 +0700

    masuk front end

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Expense Tracker</h1>
+        <h4>Track Your Spendings and Savings Here</h4>
       </header>
+
+      <table className="table table-bordered table-hover">
+        <thead>
+          <tr>
+            <th>Balance</th>
+          </tr>                   
+        </thead>
+      </table>
+
+      <table className="table table-bordered table-hover">
+        <thead>
+          <tr>
+            <th>Income</th>
+            <th>Expense</th>
+          </tr>
+        </thead>
+      </table>
+
+      <table className="table table-bordered table-hover">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Details</th>
+            <th>Label</th>
+            <th>Amounts</th>
+          </tr>
+        </thead>
+      </table>
+
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Date</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Insert Date"/>
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Details</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+
+      <div>
+      <label for="Transaction Type" class="form-label">Transaction Types</label>
+      <select class="form-select" aria-label="Default select example">
+        <option selected>--</option>
+        <option value="1">Income</option>
+        <option value="2">Expense</option>        
+      </select>
+      </div>
+
+      <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Amounts</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Normal Number for income, add (-) infront of number for Expense"/>
+      </div>
+
+      <div class="col-12">
+        <button class="btn btn-primary" type="submit">Submit</button>
+      </div>
+
     </div>
   );
 }
